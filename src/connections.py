bdeacade287a556c4e585ae552b3b24c5c733201
lokvisnu <lokvisnu@gmail.com>
2025-06-17 23:10:35 +0530

    Enhance connection handling with improved data reception and error response management

--- src/connections.py
+++ src/connections.py
@@ -1,5 +1,13 @@
+import errno
+import re
 import socket
-
+CONNECTION_STATUS={
+    "ERROR":"ERROR",
+    "SENDING_DATA":"SENDING_DATA",
+    "SHUTDOWN":"SHUTDOWN",
+    "RECIEVING_DATA":"RECIEVING_DATA",
+    "INITIALISED":"INITIALISED"
+}
 class Connection:
     def __init__(self,fd:int,is_internal:bool,conn_obj:socket.SocketType,linked_fd:int=None,rcv_data=""):
         self._fd = fd
@@ -7,7 +15,11 @@ class Connection:
         self._linked_fd = linked_fd
         self._conn_obj = conn_obj
         self._rcv_data:str = ""
+        self._recieved:str=""
         self.append_data(rcv_data=rcv_data)
+        self._bytes_sent:int = 0
+        self._response_data: None | bytes = None
+        self._status = CONNECTION_STATUS["INITIALISED"]
 
     def set_linked_fd(self,linked_fd):
         self._linked_fd = linked_fd
@@ -17,12 +29,72 @@ class Connection:
             rcv_data = rcv_data.decode('utf-8')
         self._rcv_data = rcv_data 
 
+    def recv_data(self) -> int | None:
+        self._status = CONNECTION_STATUS['RECIEVING_DATA']
+        while True:
+            try:
+                recieved: bytes = self._conn_obj.recv(1024)
+                self.append_request_data(recieved)
+                if not recieved:
+                    break
+            except BlockingIOError as e:
+                if e.errno == errno.EAGAIN:
+                    return errno.EAGAIN
+                else:
+                    raise
+
+    def append_request_data(self,recieved):
+        decoded_data = recieved
+        if self.is_binary(recieved):
+            decoded_data = recieved.decode('utf-8')
+
+        self._recieved.join(decoded_data)
+
+    def request_data(self,byt:bool=False) -> str:
+        if byt:
+            return self._recieved.encode('utf-8')
+        return self._recieved
+    
+    def send_data(self,response:bytes | str,err:bool=False) -> int:
+            if err:
+                self._status = CONNECTION_STATUS["ERROR"]
+            else:
+                self._status = CONNECTION_STATUS["SENDING_DATA"]
+
+            if not self._response_data:
+                response_bytes = response
+            else:
+                response_bytes = self._response_data
+            if not isinstance(response_bytes,bytes):
+                response_bytes = response_bytes.encode('utf-8')
+            conn_obj = self._conn_obj
+            bytes_sent = self._bytes_sent
+            while bytes_sent < len(response_bytes):
+                try:
+                    sent = conn_obj.send(response_bytes[bytes_sent:])
+                    if sent == 0:
+                        break
+                    bytes_sent += sent
+                except BlockingIOError as e:
+                    if e.errno == errno.EAGAIN:
+                        # Socket buffer full
+                        break
+                        
+                    else:
+                        raise
+            self._bytes_sent = bytes_sent
+            self._response_data = response_bytes[bytes_sent:]
+            return bytes_sent
+    
     def append_data(self,rcv_data) -> None:
         decoded_rcv_data = rcv_data
         if self.is_binary(rcv_data):
             decoded_rcv_data = rcv_data.decode('utf-8')
         self._rcv_data = self._rcv_data.join(decoded_rcv_data)
 
+    def status(self)->str:
+        return self._status
+    
     def fileno(self) -> int:
         return self._fd
     
@@ -34,7 +106,14 @@ class Connection:
         """returns the FD of the linked internal or external connection"""
         return self._linked_fd
     
-    def get_connection_obj(self)->socket.SocketType:
+    def shutdown(self,mode) -> None:
+        self._status = CONNECTION_STATUS['SHUTDOWN']
+        self._conn_obj.shutdown(mode)
+
+    def close(self):
+        self._conn_obj.close()
+
+    def connection(self)->socket.SocketType:
         """returns the Socket object associated with the connection"""
         return self._conn_obj
     
--- src/worker_process.py
+++ src/worker_process.py
@@ -1,8 +1,10 @@
+from ast import mod
+import errno
 import select,socket
 from stat import filemode
 from typing import List
 import yaml
-from connections import Connection,ConnectionType
+from connections import CONNECTION_STATUS, Connection,ConnectionType
 # try to import C parser then fallback in pure python parser.
 try:
     from http_parser.parser import HttpParser
@@ -62,73 +64,105 @@ class WorkerProcess():
                        self.accept_incomming_connection()
 
                     elif event & select.EPOLLIN: # socket has incoming stream
-                        self.recieve_incomming_data(fileno=fileno)
-                        if EOL1 in self._connection_data[fileno].get_binary_rcv_data() or EOL2 in self._connection_data[fileno].get_binary_rcv_data(): # EOL1 & EOL2 indicates end of recieved data
+                        # self.recieve_incomming_data(fileno=fileno)
+                        self._connection_data[fileno].recv_data()
+                        recieved_data = self._connection_data[fileno].request_data(byt=True)
+                        # EOL1 & EOL2 indicates end of recieved data
+                        if EOL1 in recieved_data or EOL2 in recieved_data: 
                             if self._connection_data[fileno].is_internal():
                                 # internal connection with upstream server 
-                                self._connection_data[fileno].get_connection_obj().shutdown(socket.SHUT_RDWR)
+                                self._connection_data[fileno].shutdown(mode=socket.SHUT_RDWR) # shutsdown connection with the internal server
                                 linked_fd = self._connection_data[fileno].linked_fd()
+                                #Modify epoll to listen for termination
                                 self._epoll.modify(fd=fileno,eventmask=select.EPOLLET)
-                                self._epoll.modify(fd=linked_fd,eventmask=select.EPOLLOUT | select.EPOLLET) # modify registered fd
+                                #Modify epoll to listen for EPOLLOUT( Lvl Triggered)
+                                self._epoll.modify(fd=linked_fd,eventmask=select.EPOLLOUT)
                             else:
                                 # external client connection 
                                 self.forward_to_internal_upstreams(fileno=fileno)
 
                     elif event & select.EPOLLOUT: # socket ready for output stream
-                        linked_fd = self._connection_data[fileno].linked_fd()
-                        output_stream_data = self._connection_data[linked_fd].get_binary_rcv_data()
-                        try:
-                            while len(output_stream_data)>0:
-                                bytes_written = self._connection_data[fileno].get_connection_obj().send(output_stream_data)
-                                output_stream_data = output_stream_data[bytes_written:]
-                        except socket.error:
-                            pass
-                        self._connection_data[linked_fd].set_rcv_data(output_stream_data) 
+                        if self._connection_data[fileno].status() == CONNECTION_STATUS['ERROR']:
+                            self.send_http_error(fileno=fileno)
+                        else:
+                            linked_fd = self._connection_data[fileno].linked_fd()
+                            response_data = self._connection_data[linked_fd].request_data(byt=True)
+                            sent_bytes = self._connection_data[fileno].send_data(response=response_data)
+                            # Store remaining response data
+                            if sent_bytes == len(response_data):
+                                if not self._connection_data[fileno].is_internal():
+                                    #Shutdown external connection
+                                    self._connection_data[fileno].shutdown(mode=socket.SHUT_RDWR)
+                                    #Modify epoll to wait for shutdown
+                                    self._epoll.modify(fd=fileno,eventmask=select.EPOLLET)
+                                else:
+                                    self._epoll.modify(fd=fileno,eventmask=select.EPOLLIN | select.EPOLLET)
+                            else :
+                                # Socket buffer full, modify epoll to wait for EPOLLOUT
+                                self._epoll.modify(fd=fileno,eventmask=select.EPOLLOUT | select.EPOLLET)
 
-                        if not self._connection_data[fileno].is_internal() and len(output_stream_data) == 0 :
-                            self._epoll.modify(fd=fileno,eventmask=select.EPOLLET)
-                            # self._epoll.modify(fd=linked_fd,eventmask=select.EPOLLET)
-                            self._connection_data[fileno].get_connection_obj().shutdown(socket.SHUT_RDWR)
-                            # self._connection_data[linked_fd].get_connection_obj().shutdown(socket.SHUT_RDWR)
-
-                        elif self._connection_data[fileno].is_internal() and len(output_stream_data) == 0:
-                            self._epoll.modify(fd=fileno,eventmask=select.EPOLLIN | select.EPOLLET)
-                        pass
 
                     elif event & select.EPOLLHUP: # hang-up event
                         if fileno in self._connection_data:
                             self._epoll.unregister(fd=fileno)
-                            self._connection_data[fileno].get_connection_obj().close()
+                            self._connection_data[fileno].close()
                             if not self._connection_data[fileno].is_internal():
                                 del self._connection_data[self._connection_data[fileno].linked_fd()]
                                 del self._connection_data[fileno]
         except Exception as e:
             print(e)
+
         finally:
             self._epoll.unregister(self._server_socket_fd)
             self._epoll.close()
 
+  
+
+    def send_data(self,fileno,response):
+            if fileno in self._connection_data:
+                conn_obj: socket.SocketType = self._connection_data[fileno].connection()
+                response_bytes = response
+                if not isinstance(response_bytes,bytes):
+                    response_bytes = response.encode('utf-8')
+                
+                bytes_sent = 0
+                while bytes_sent < len(response_bytes):
+                    try:
+                        sent = conn_obj.send(response_bytes[bytes_sent:])
+                        if sent == 0:
+                            break
+                        bytes_sent += sent
+                    except BlockingIOError as e:
+                        if e.errno == errno.EAGAIN:
+                            break
+                        else:
+                            raise
+                return bytes_sent
+                
+
     def recieve_incomming_data(self,fileno) -> None: 
          # recieve incoming data stream from the connection and write to connection data
-         try:
-            conn_obj = self._connection_data[fileno].get_connection_obj()
-            while True:
-                rcv_data = conn_obj.recv(RECV_BYTES)
-                if rcv_data:
+         conn_obj = self._connection_data[fileno].connection()
+         while True:
+            try:
+                    rcv_data = conn_obj.recv(RECV_BYTES)
+                    if not rcv_data:
+                        break
                     self._connection_data[fileno].append_data(rcv_data=rcv_data)
-                else:
+            except BlockingIOError as e: 
+                if e.errno == errno.EAGAIN:
                     break
-                # print(f"Recieving Data: {self._connection_data[fileno].get_rcv_data()}")
-         except socket.error: # data recieved sucessfully
-            pass
+                else:
+                    raise
 
     def accept_incomming_connection(self) -> None:
          
          try:
             while True:
-                conn,addr = self._server_socket.accept()
+                conn,__ = self._server_socket.accept()
                 conn.setblocking(False)
-                self._epoll.register(conn.fileno(),select.EPOLLIN | select.EPOLLET)
+                #Register epoll, listen to incoming data
+                self._epoll.register(conn.fileno(),select.EPOLLIN | select.EPOLLET) 
                 self._connection_data[conn.fileno()]  = Connection(fd=conn.fileno(),is_internal=False,conn_obj=conn)
          except socket.error:
             pass
@@ -152,38 +186,103 @@ class WorkerProcess():
                 request_path = http_parser.get_path()
                 matched_path = self.longest_prefix_match_path(request_path=request_path)
                 if matched_path:
-                    #longest matching path found
-                    redirect_rule = self._redirect_rules[matched_path] 
-                    upstream_server_ids = redirect_rule["upstreams"]
+                    #Longest matching path found
+                    redirect_rule:dict = self._redirect_rules.get(matched_path)
+                    upstream_server_ids:List = redirect_rule.get("upstreams")
                     # load balancing code goes here
-                    selected_upstream_server = self._upstream_server[upstream_server_ids[0]] # selecting upstream server at index 0
+                    # selecting upstream server at index 0
+                    selected_upstream_server = self._upstream_server.get(upstream_server_ids[0])
                     try:
                         upstream_socket = socket.socket(family=socket.AF_INET,type=socket.SOCK_STREAM)
                         upstream_socket.setblocking(False)
                         try:
                             upstream_socket.connect((selected_upstream_server["host"],selected_upstream_server["port"]))
                         except socket.error as e:
-                            print(e)
+                            if not e.errno == errno.EINPROGRESS:
+                                raise
                         self._connection_data[fileno].set_linked_fd(linked_fd=upstream_socket.fileno())
                         upstream_connection = Connection(is_internal=True,conn_obj=upstream_socket,linked_fd=fileno,fd=upstream_socket.fileno())
-                        self._connection_data[upstream_socket.fileno()] = upstream_connection
+                        self._connection_data.__setattr__(name=upstream_socket.fileno(),value=upstream_connection)
 
-                        ## register for EPOLLOUT( connection established & ready for streaming data) in the socket
+                        #Register Epoll to wait for EPOLLOUT in internal connection
                         self._epoll.register(fd=upstream_socket.fileno(),eventmask=select.EPOLLOUT | select.EPOLLET)
                     except Exception as e:
                         print(f"Error forwarding request to upstream server: {e}")
-                        pass # Internal Server Error
+                        self.send_http_error(fileno, 502, "Bad Gateway")  # Internal Server Error
                 else:
-                    pass # 404 not found
+                    self.send_http_error(fileno, 404)  # 404 not found
             else:
-                pass # 404 not found
+                self.send_http_error(fileno, 404)  # 404 not found
         except  Exception as e:
-            # deregister connection FD
-            # send appropriate response 
-            # end external connection 
             print(f"HTTP Request Parsing Error: {e}")
+            self.send_http_error(fileno, 400, "Bad Request")  # Bad Request for parsing errors
         pass
 
+    def send_http_error(self, fileno: int, status_code: int = 400, reason_phrase: str = None) -> None:
+        """
+        Sends an HTTP error response to the client connection identified by fileno.
+        Compatible with epoll non-blocking operations.
+        
+        Args:
+            fileno: File descriptor of the client connection
+            status_code: Optional HTTP status code (404, 500, etc.)
+            reason_phrase: Optional custom reason phrase
+        """
+        # Standard HTTP status messages
+        status_messages = {
+            400: "Bad Request",
+            404: "Not Found", 
+            500: "Internal Server Error",
+            502: "Bad Gateway",
+            503: "Service Unavailable",
+            504: "Gateway Timeout"
+        }
+        
+        if reason_phrase is None:
+            reason_phrase = status_messages.get(status_code, "Error")
+        
+        # Create HTTP error response
+        error_body = f"""<!DOCTYPE html>
+                    <html>
+                    <head><title>{status_code} {reason_phrase}</title></head>
+                    <body>
+                    <h1>{status_code} {reason_phrase}</h1>
+                    <p>The requested resource could not be found or an error occurred.</p>
+                    </body>
+                    </html>"""
+                
+        response = "".join((
+            f"HTTP/1.1 {status_code} {reason_phrase}\r\n",
+            "Content-Type: text/html\r\n",
+            f"Content-Length: {len(error_body)}\r\n",
+            "Connection: close\r\n",
+            "\r\n",
+            error_body))
+        
+        try:
+            if fileno in self._connection_data:
+                response_bytes = response.encode('utf-8')
+                bytes_sent:int = self._connection_data[fileno].send_data(response=response_bytes,err=True)
+                response_bytes = response_bytes[bytes_sent:]
+                if not len(response_bytes) == 0:
+                    # Response yet to be sent
+                    self._epoll.modify(fd=fileno,eventmask= select.EPOLLOUT | select.EPOLLET)
+                else:
+                    # Close connection after sending error
+                    self._epoll.modify(fd=fileno, eventmask=select.EPOLLET)
+                    self._connection_data[fileno].shutdown(socket.SHUT_RDWR)
+                
+        except Exception as e:
+            print(f"Error sending HTTP error response: {e}")
+            # Force close connection on any error
+            if fileno in self._connection_data:
+                try:
+                    self._epoll.unregister(fd=fileno)
+                    self._connection_data[fileno].close()
+                    del self._connection_data[fileno]
+                except:
+                    pass
+    
     def longest_prefix_match_path(self, request_path) -> str | None:
         for path in self._allowed_paths:
             if request_path.startswith(path):
